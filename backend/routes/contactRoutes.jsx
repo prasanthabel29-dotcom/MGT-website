60666c4c47f31ade201c6e249bd1208b70e888c1
prasanth <prasanthabel29@gmail.com>
2026-07-17 11:34:01 +0530

    backendSeperated

--- backend/routes/contactRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { submitContact, fetchContacts } from "../controllers/contactController.jsx";
-
-const router = express.Router();
-
-router.post("/contact", submitContact);
-router.get("/contacts", fetchContacts);
-
-export default router;
